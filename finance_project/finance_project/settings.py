2c280509af540c9bec94461674626ec9f10bcdff
HARDROCKERZ <mihir.rockerz@gmail.com>
2022-12-20 18:39:51 +0530

    Tweaks in Settings

--- finance_project/finance_project/settings.py
+++ finance_project/finance_project/settings.py
@@ -121,10 +121,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
+
+# MEDIA_URl = '/static/img/'
+# MEDIA_ROOT = '/static/img/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
